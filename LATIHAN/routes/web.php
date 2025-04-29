4622c2a4b7645eaaebe722499a7753656d48235a
berlianwanna <berlian.wanna@gmail.com>
2025-04-29 20:36:53 +0700

    belajar view dan route

--- LATIHAN/routes/web.php
+++ LATIHAN/routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', function () {
+    return view('login', ['name' => 'berlian wanna',
+                          'email' => 'berlian.wanna@gmail.com',
+                          'alamat' => 'RamaKasih IV'
+]
+);
+});
+Route::get('/berita/{id}/{judul?}', function ($id, $judul = judul) {
+    return view('berita', ['id' => $id, 'judul' => $judul]);
+});
